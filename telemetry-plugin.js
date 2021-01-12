8ef86a49abc5c8e7f507f17bbe06772906b68d4e
A731 <audunelv@hotmail.com>
2021-01-12 15:19:22 +0100

    Added base files needed for telemetry server

---
+++ telemetry-plugin.js
@@ -0,0 +1,61 @@
+/**
+ * Basic historical telemetry plugin.
+ 
+var HistoryServer = './history-server';
+var spacecraft = new Spacecraft();
+//var historyServer = new HistoryServer(spacecraft);
+app.use('/history', historyServer);
+*/
+function HistoricalTelemetryPlugin() {
+    return function install (openmct) {
+        var provider = {
+            supportsRequest: function (domainObject) {
+                return domainObject.type === 'example.telemetry';
+            },
+            request: function (domainObject, options) {
+                var url = 'http://localhost/node/history/' +
+                    domainObject.identifier.key +
+                    '?start=' + options.start +
+                    '&end=' + options.end;
+
+                return http.get(url)
+                    .then(function (resp) {
+                        return resp.data;
+                    });
+            }
+        };
+
+        openmct.telemetry.addProvider(provider);
+    }
+}
+
+
+function RealtimeTelemetryPlugin() {
+    return function (openmct) {
+        var socket = new WebSocket(location.origin.replace(/^http/, 'ws') + ':8080/realtime/');
+	var listener = {};
+
+        socket.onmessage = function (event) {
+            point = JSON.parse(event.data);
+            if (listener[point.id]) {
+                listener[point.id](point);
+            }
+        };
+
+        var provider = {
+            supportsSubscribe: function (domainObject) {
+                return domainObject.type === 'example.telemetry';
+            },
+            subscribe: function (domainObject, callback) {
+                listener[domainObject.identifier.key] = callback;
+                socket.send('subscribe ' + domainObject.identifier.key);
+                return function unsubscribe() {
+                    delete listener[domainObject.identifier.key];
+                    socket.send('unsubscribe ' + domainObject.identifier.key);
+                };
+            }
+        };
+
+        openmct.telemetry.addProvider(provider);
+    }
+}
