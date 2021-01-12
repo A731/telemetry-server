39e3240b65a6e224987b186062c0a9602f63b83c
A731 <audunelv@hotmail.com>
2021-01-12 16:43:36 +0100

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,1 +1,25 @@
-
+apt-get update -y && apt-get upgrade -y
+apt-get install -y git npm nodejs apache2
+rm -rf /var/www/html/*
+cd /var/www/html/
+git clone https://github.com/A731/telemetry-server.git /var/www/html
+npm install openmct
+npm install express
+npm install express-ws
+npm install node-fetch
+npm install -g node-gyp
+npm install -g node-pre-gyp
+npm install serialport --unsafe-perm
+npm install -g pm2
+mv CUSTOM.conf /etc/apache2/mods-available
+cd /var/www/html/node-modules/serialport
+node-gyp configure build
+cd /var/www/html/
+pm2 start /var/www/html/server.js
+pm2 startup systemd
+a2enmod mod_proxy
+a2enmod proxy_http
+a2enmod proxy_wstunnel
+a2enconf CUSTOM
+service apache2 restart
+echo "\nDone!"
