b729bc05141c67f4ee741894a759a2564327dca9
A731 <audunelv@hotmail.com>
2021-01-13 10:30:13 +0100

    Update install.sh

--- install.sh
+++ install.sh
@@ -2,7 +2,7 @@ apt-get update -y && apt-get upgrade -y
 apt-get install -y git npm nodejs apache2
 rm -rf /var/www/html/*
 cd /var/www/html/
-git clone https://github.com/A731/telemetry-server.git /var/www/html
+git clone https://github.com/A731/telemetry-server.git /var/www/.temp
 npm install openmct
 npm install express
 npm install express-ws
@@ -15,6 +15,8 @@ mv CUSTOM.conf /etc/apache2/mods-available
 cd /var/www/html/node-modules/serialport
 node-gyp configure build
 cd /var/www/html/
+cp /var/www/.temp/* /var/www/html/
+rm -rf /var/www/.temp
 pm2 start /var/www/html/server.js
 pm2 startup systemd
 a2enmod mod_proxy
@@ -22,4 +24,4 @@ a2enmod proxy_http
 a2enmod proxy_wstunnel
 a2enconf CUSTOM
 service apache2 restart
-echo "\nDone!"
+echo "\nDone!\n"
