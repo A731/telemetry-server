e2662881bda7236578125d953980e92432405cd8
A731 <audunelv@hotmail.com>
2021-02-25 17:55:44 +0100

    Update install.sh

--- install.sh
+++ install.sh
@@ -2,6 +2,8 @@ apt-get update -y && apt-get upgrade -y
 apt-get install -y git npm nodejs apache2
 rm -rf /var/www/html/*
 cd /var/www/html/
+git clone https://github.com/nasa/openmct.git
+npm install
 git clone https://github.com/A731/telemetry-server.git /var/www/.temp
 npm install openmct
 npm install express
@@ -11,7 +13,7 @@ npm install -g node-gyp
 npm install -g node-pre-gyp
 npm install serialport --unsafe-perm
 npm install -g pm2
-mv CUSTOM.conf /etc/apache2/mods-available
+mv /var/www/html/CUSTOM.conf /etc/apache2/conf-available/
 cd /var/www/html/node-modules/serialport
 node-gyp configure build
 cd /var/www/html/
@@ -19,7 +21,7 @@ cp /var/www/.temp/* /var/www/html/
 rm -rf /var/www/.temp
 pm2 start /var/www/html/server.js
 pm2 startup systemd
-a2enmod mod_proxy
+a2enmod proxy
 a2enmod proxy_http
 a2enmod proxy_wstunnel
 a2enconf CUSTOM
