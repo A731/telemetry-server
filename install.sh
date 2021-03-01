5a3277c1535455cd1b7e139eeb14a3fde65d1f64
A731 <audunelv@hotmail.com>
2021-03-01 02:08:33 +0100

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,13 +1,11 @@
-apt-get update -y && apt-get upgrade -y
-apt-get install -y git npm nodejs apache2 python python3
-rm -rf /var/www/html/*
-cd /var/www/html/
+cd ~/
+sudo apt-get update -y && apt-get upgrade -y
+sudo apt-get install -y git npm nodejs apache2 python python3
+sudo rm -rf /var/www/html/*
 git clone https://github.com/nasa/openmct.git
+git clone https://github.com/A731/telemetry-server.git ~/.temp
+cd openmct
 npm install
-git clone https://github.com/A731/telemetry-server.git /var/www/.temp
-npm install openmct
-mv /var/www/html/openmct/* /var/www/html/
-rm -rf /var/www/html/openmct
 npm install express
 npm install express-ws
 npm install node-fetch
@@ -16,17 +14,20 @@ npm install -g node-gyp
 npm install -g node-pre-gyp
 npm install serialport --unsafe-perm
 npm install -g pm2
-cd /var/www/html/node-modules/serialport
-node-gyp configure build
+sudo mv -R ~/openmct/* /var/www/html/
+sudo cp -R /var/www/html/dist/* /var/www/html/
 cd /var/www/html/
-cp /var/www/.temp/* /var/www/html/
+cd /var/www/html/node-modules/serialport
+sudo node-gyp configure build
+cd /var/www/html/
+sudo cp ~/.temp/* /var/www/html/
 mv /var/www/html/CUSTOM.conf /etc/apache2/conf-available/
-rm -rf /var/www/.temp
-pm2 start /var/www/html/server.js
-pm2 startup systemd
-a2enmod proxy
-a2enmod proxy_http
-a2enmod proxy_wstunnel
-a2enconf CUSTOM
-service apache2 restart
+sudo rm -rf /var/www/.temp
+sudo pm2 start /var/www/html/server.js
+sudo pm2 startup systemd
+sudo a2enmod proxy
+sudo a2enmod proxy_http
+sudo a2enmod proxy_wstunnel
+sudo a2enconf CUSTOM
+sudo service apache2 restart
 echo "\nDone!\n"
